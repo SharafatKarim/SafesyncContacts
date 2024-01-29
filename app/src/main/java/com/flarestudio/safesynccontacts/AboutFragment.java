08ab2c6d973759c4090d9b9bf798065e8a96ddde
SharafatKarim <sharafat2004@gmail.com>
2024-01-29 10:19:02 +0600

    About fragment part completed 🔌

--- app/src/main/java/com/flarestudio/safesynccontacts/AboutFragment.java
+++ app/src/main/java/com/flarestudio/safesynccontacts/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.flarestudio.safesynccontacts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        Button github = view.findViewById(R.id.GitHub_REPO);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String GITHUB_URL = "https://github.com/SharafatKarim/SafeSyncContacts";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_URL));
+                startActivity(intent);
+            }});
+
+        Button issue = view.findViewById(R.id.Issue_SUBMISSION);
+        issue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ISSUE_URL = "https://github.com/SharafatKarim/SafeSyncContacts/issues";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ISSUE_URL));
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
